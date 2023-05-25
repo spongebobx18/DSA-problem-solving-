d99fec1ed5125fbcb6fe2468e2f2fdb1ad513121
spongebobx18 <128106462+spongebobx18@users.noreply.github.com>
2023-05-25 20:42:47 +0000

    Add files via upload

---
+++ wierd.py
@@ -0,0 +1,13 @@
+def weird(n):
+    if n % 2 != 0:
+        print("Weird")
+    elif n >= 2 and n <= 5:
+        print("Not Weird")
+    elif n >= 6 and n <= 20:
+        print("Weird")
+    else:
+        print("Not Weird")
+
+
+n = int(input("Enter a positive integer: "))
+weird(n)
