d0bf8e0b6d3b2560ca4f93a91a4fce0a04e45f65
spongebobx18 <128106462+spongebobx18@users.noreply.github.com>
2023-05-30 18:40:56 +0000

    Add files via upload

---
+++ countprime.py
@@ -0,0 +1,22 @@
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+
+    return True
+
+def count_primes(n):
+    count = 0
+    for i in range(n):
+        if is_prime(i):
+            count += 1
+    return count
