a6ab085351278f9948852d23a07fdbb6a63973be
spongebobx18 <128106462+spongebobx18@users.noreply.github.com>
2023-06-18 17:22:11 +0000

    Update transposematrix.py

--- transposematrix.py
+++ transposematrix.py
@@ -4,7 +4,7 @@ class Solution:
     
         rows = len(matrix)
         cols = len(matrix[0])
-        transposed = [[0 for _ in range(rows)] for _ in range(cols)]
+        transposed = [[0 for _ in range(rows)] for _ in range(cols)]# swapping sizes ensure that we get the correct shape in the transposed matrix everytime
         for i in range(rows):
             for j in range(cols):
                 transposed[j][i] = matrix[i][j]
