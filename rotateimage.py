df7a751f12fb6c72c336ef5ce1a5eb16006f7f94
spongebobx18 <128106462+spongebobx18@users.noreply.github.com>
2023-06-27 00:03:13 +0000

    Add files via upload

---
+++ rotateimage.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+
+        n = len(matrix)
+
+        # Transpose the matrix
+        for i in range(n):
+            for j in range(i , n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+        # Reverse each row
+        for i in range(n):
+            matrix[i] = matrix[i][::-1]
