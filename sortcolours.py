e9d411b90c1ebfdd2545650f6c50148357e18377
spongebobx18 <128106462+spongebobx18@users.noreply.github.com>
2023-07-04 12:24:48 +0000

    Add files via upload

---
+++ sortcolours.py
@@ -0,0 +1,8 @@
+def sortColors(self, nums: list[int]) -> None:
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    for i in range(len(nums) - 1):
+        for j in range(len(nums) - 1):
+            if nums[j] > nums[j + 1]:
+                nums[j], nums[j + 1] = nums[j + 1], nums[j]
