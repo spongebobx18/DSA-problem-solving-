ce72af72d2c531f024aa7b41fc220ade304dc668
spongebobx18 <128106462+spongebobx18@users.noreply.github.com>
2023-09-04 11:44:30 +0000

    Add files via upload

---
+++ palidrome_linkedlist.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        fast=head
+        slow=head
+        arr=[]
+        while fast and fast.next:
+            fast=fast.next.next
+            slow=slow. 
+        prev=None
+        while slow:
+            tmp=slow.next
+            slow.next=prev
+            prev=slow
+            slow=tmp
+        left, right=head, prev
+        while right:
+            if left.val!=right.val:
+
+
+                return False 
+            left=left.next
+            right=right.next
+        return True
+
+        
