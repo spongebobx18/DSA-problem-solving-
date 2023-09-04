581a6288a5687854c455c678f993a9495baef06c
spongebobx18 <128106462+spongebobx18@users.noreply.github.com>
2023-09-04 12:53:47 +0000

    Add files via upload

---
+++ reversebetween.py
@@ -0,0 +1,23 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        dummy=ListNode(0, head)
+        leftprev, cur = dummy , head 
+        for i in range (left-1):
+            leftprev, cur = cur, cur.next 
+        prev =None
+        for i in range (right-left+1):
+            tmpNext=cur.next
+            cur.next=prev
+            prev, cur=cur, tmpNext
+        leftprev.next.next=cur
+        leftprev.next=prev
+        ans=dummy.next
+        return ans
+
+
+        
