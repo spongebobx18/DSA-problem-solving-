4ffa10eb9ecf99b98bb9ea2a9bb1722e611ed09c
spongebobx18 <128106462+spongebobx18@users.noreply.github.com>
2023-09-06 11:02:01 +0000

    Add files via upload

---
+++ partitionlist.py
@@ -0,0 +1,25 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        left, right =ListNode(), ListNode()
+        ltail, rtail= left, right 
+        while head :
+            if head.val<x:
+                ltail.next=head 
+                ltail=ltail.next
+                print(ltail.val)
+            else:
+                rtail.next=head
+                rtail=rtail.next
+                print(rtail.val)
+            head=head.next
+        ltail.next=right.next
+        rtail.next=None
+        return left.next
+
+        
+        
