5ea355b6abb81a31db98cb0c47c9d17953ae56c9
spongebobx18 <128106462+spongebobx18@users.noreply.github.com>
2023-09-07 16:25:58 +0000

    Add files via upload

---
+++ minstack.py
@@ -0,0 +1,33 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack=[]
+        
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        
+
+    def pop(self) -> None:
+        self.stack.pop(-1)
+        
+
+    def top(self) -> int:
+        if not self.stack:
+            return 
+
+        return self.stack[-1]
+        
+
+    def getMin(self) -> int:
+        return min(self.stack)
+
+        
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
