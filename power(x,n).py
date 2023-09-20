43bd095dada6eed95f20d494d3a6f76f870a3f07
spongebobx18 <128106462+spongebobx18@users.noreply.github.com>
2023-09-20 16:09:51 +0000

    Add files via upload

---
+++ power(x,n).py
@@ -0,0 +1,9 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n==0:
+            return 1
+        
+            if n==1:
+                return x
+        return (x**n)
+        
