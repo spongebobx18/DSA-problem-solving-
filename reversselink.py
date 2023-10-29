16cefee6ebb4e64ef45b4b048c159ea75df4682e
Prince Benneh <128106462+spongebobx18@users.noreply.github.com>
2023-10-29 01:11:49 +0000

    Add files via upload

---
+++ reversselink.py
@@ -0,0 +1,18 @@
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+       
+        if head==None:
+            return head
+        if head.next==None:
+            return head
+        nexthead = self.reverseList(head.next)
+        head.next.next = head
+        head.next = None
+        return nexthead
